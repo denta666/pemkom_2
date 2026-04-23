dfbc7e8ded361d1d723388fe2c88b4e5c3d2dd1d
denta666 <skaterset@gmail.com>
2026-04-23 12:47:41 +0700

    first commit

---
+++ src/main/java/sistem_absensi/Sistem_absensi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistem_absensi;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sistem_absensi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
